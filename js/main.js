7f13c7051361b7f3dc3d55e2d2e5f40d3601d7d3
Auron Hines <auronhines@gmail.com>
2018-11-12 09:34:21 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -23,9 +23,9 @@ rightBtn = document.createElement('button');
 rightBtn.innerText = ">";
 fireBtn = document.createElement('button');
 fireBtn.innerText = "*";
-leftBtn.className = "btn-primary";
-rightBtn.className = "btn-primary";
-fireBtn.className = "btn-primary";
+leftBtn.className = "btn btn-primary";
+rightBtn.className = "btn btn-primary";
+fireBtn.className = "btn btn-primary";
 div = document.createElement('div');
 div.appendChild(leftBtn);
 div.appendChild(fireBtn)
