510937d859ddfd259c9b9459d683b4a3eed72854
Victor Ribeiro <victorqribeiro@gmail.com>
2018-11-20 03:51:31 -0200

    fix service worker

--- js/main.js
+++ js/main.js
@@ -193,12 +193,38 @@ function addEvents(){
 		player.isMovingRight = false;
 	});
 	
-	window.applicationCache.addEventListener('updateready', function(e) {
-		if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
-			window.applicationCache.swapCache();
-			window.location.reload();
-		}
+	window.addEventListener('load', function(e) {
+
+		window.applicationCache.addEventListener('updateready', function(e) {
+		  if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+				window.applicationCache.swapCache();
+				window.location.reload();
+		  }
+		}, false);
+
 	}, false);
+
+
+	if('serviceWorker' in navigator) {
+		navigator.serviceWorker
+		         .register('/invaderz/sw.js')
+		         .then(function() { });
+	}
+
+	let deferredPrompt;
+	const addBtn = document.createElement('button');
+
+	window.addEventListener('beforeinstallprompt', (e) => {
+		e.preventDefault();
+		deferredPrompt = e;
+		addBtn.addEventListener('click', (e) => {
+		  addBtn.style.display = 'none';
+		  deferredPrompt.prompt();
+		  deferredPrompt.userChoice.then((choiceResult) => {
+		      deferredPrompt = null;
+		    });
+		});
+	});
 	
 }
 
